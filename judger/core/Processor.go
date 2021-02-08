7b96ee4a96f493d899f33ebcbbe53f0e27a05032
chenzitao <348673210@qq.com>
2021-02-08 16:21:39 +0800

    完成校验

--- judger/core/Processor.go
+++ judger/core/Processor.go
@@ -1,13 +1,19 @@
 package core
 
 import (
+	"bufio"
 	"bytes"
+	"errors"
+	"fmt"
+	"io"
 	"log"
+	"os"
 	"os/exec"
 	proto "snail/judger/grpcServer"
 	"snail/judger/model"
 	"snail/judger/settings"
 	"strconv"
+	"strings"
 )
 
 var (
@@ -78,7 +84,7 @@ func compile(wordPath string) (result int, msg string) {
 	return 0, "编译成功"
 }
 
-// TODO check answer
+// TODO 保存记录
 func runJudge(submission *model.Submission, workPath string) (msg string) {
 	queId := submission.ProblemId
 	checkPointList, err := model.GetCheckPointByProblemId(queId)
@@ -98,12 +104,20 @@ func runJudge(submission *model.Submission, workPath string) (msg string) {
 		log.Printf("get question failed: %v\n", err)
 		return err.Error()
 	}
+	totalMemory := 0
+	totalTime := 0
+	count := 0
+	pass := 0
 	for index, checkPoint := range checkPointList {
+		count += 1
 		scriptName, err := genCheckScript(index, language.RunCommand, language.ExeFileName, checkPoint.Input, question, workPath)
 		if err != nil {
 			OnErrorOccurred("执行第" + strconv.Itoa(index) + "个测试用例失败" + err.Error())
 			continue
 		}
+		result := workPath + "/result_" + strconv.Itoa(index) + ".txt"
+		timeLog := workPath + "/time_log_" + strconv.Itoa(index) + ".txt"
+		memoryLog := workPath + "/memory_log_" + strconv.Itoa(index) + ".txt"
 		cmd := exec.Command("sh", scriptName)
 		var out bytes.Buffer
 		cmd.Stdout = &out
@@ -115,7 +129,103 @@ func runJudge(submission *model.Submission, workPath string) (msg string) {
 			log.Printf("run cmd error: %v\n", err)
 			OnErrorOccurred("执行第" + strconv.Itoa(index) + "个测试用例失败" + err.Error())
 		}
-		OnOneCheckPointFinished("执行第" + strconv.Itoa(index) + "成功")
+		ret, code, err := checkAnswer(result, checkPoint.Output)
+		if err != nil {
+			log.Printf("check answer failed: %v\n", err)
+			return err.Error()
+		}
+		if code == 0 {
+			pass += 1
+		}
+		OnOneCheckPointFinished("执行第" + strconv.Itoa(index) + "个样例完成,结果:" + ret)
+		addCostTime(timeLog, &totalTime)
+		addMemory(memoryLog, &totalMemory)
 	}
-	return "success"
+	return "样例通过情况:" + strconv.Itoa(pass) + "/" + strconv.Itoa(count) +
+		"; 平均耗时:" + strconv.Itoa(totalTime/count) +
+		"; 占用内存" + strconv.Itoa(totalMemory/count)
+}
+
+func checkAnswer(resultPath string, output string) (msg string, code int, err error) {
+	result, err := os.Open(resultPath)
+	if err != nil {
+		log.Printf("Open result file failed: %v\n", err)
+		return "no result", -1, err
+	}
+	defer result.Close()
+	reader := bufio.NewReader(result)
+	index := 0
+	var stringBuilder strings.Builder
+	for {
+		str, err := reader.ReadString('\n') //读到一个换行就结束
+		if index == 0 {
+			switch str {
+			case "-1":
+				return "超出内存限制", -1, errors.New("-1")
+			case "-2":
+				return "超出时间限制", -1, errors.New("-2")
+			}
+		}
+		if err == io.EOF { //io.EOF 表示文件的末尾
+			break
+		}
+		stringBuilder.WriteString(str)
+		fmt.Print(str)
+		index++
+	}
+	if stringBuilder.String() == output {
+		return "答案正确", 0, nil
+	} else {
+		return "答案错误", -1, nil
+	}
+}
+
+func addCostTime(timeLogPath string, totalTime *int) {
+	result, err := os.Open(timeLogPath)
+	if err != nil {
+		log.Printf("read time file failed: %v\n", err)
+	}
+	defer result.Close()
+	reader := bufio.NewReader(result)
+	last := ""
+	for {
+		str, err := reader.ReadString('\n') //读到一个换行就结束
+		if err == io.EOF {                  //io.EOF 表示文件的末尾
+			break
+		}
+		fmt.Print(str)
+		last = strings.Trim(str, "\n")
+	}
+	ret, err := strconv.Atoi(last)
+	if err != nil {
+		log.Printf("convert string failed: %v\n", err)
+	}
+	*totalTime += ret
+}
+
+func addMemory(memoryLogPath string, totalMemory *int) {
+	result, err := os.Open(memoryLogPath)
+	if err != nil {
+		log.Printf("read memory file failed: %v\n", err)
+		return
+	}
+	defer result.Close()
+	reader := bufio.NewReader(result)
+	total := 0
+	count := 0
+	for {
+		str, err := reader.ReadString('\n') //读到一个换行就结束
+		if err == io.EOF {                  //io.EOF 表示文件的末尾
+			break
+		}
+		fmt.Print(str)
+		tmp, err := strconv.Atoi(strings.Trim(str, "\n"))
+		if err != nil {
+			log.Printf("convert string failed: %v\n", err)
+			return
+		}
+		total += tmp
+		count += 1
+	}
+	*totalMemory += total / count
 }
